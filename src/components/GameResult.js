a506771d80bbb9f172617c55927a194926199847
logical-1985516 <ngyanjie0418@gmail.com>
2023-07-30 16:21:01 +0800

    Organise and add styles and new metrics to gameResult
    Added new metrics: clicks per second, useful clicks per second,
    throughput, correctness, RQP and IOS. The latter 4 can be searched under
    minesweeper guides, while the former 2 are self-explanatory. Used styles
    to split these information into 2 sections: game statistics and metrics.
    The game statistics are further divided into time, progress and clicks,
    while the metrics are divided based on which of the 2 above statistics
    are used (technically correctness does not use progress, but it still
    measures the clicking efficiency).

--- src/components/GameResult.js
+++ src/components/GameResult.js
@@ -1,32 +1,89 @@
 import React from "react"
 
 export default function GameResult(props) {
-    const efficiency = 100 * props.current3BV / (props.usefulClicks + props.wastedClicks)
+    const gameProgress = props.current3BV / props.threeBV
+    const estimatedTime = props.time / gameProgress
     const threeBVPerSecond = props.current3BV / props.time
+    const totalClicks = props.usefulClicks + props.wastedClicks
+    const RQP = props.time / threeBVPerSecond
+    const IOS = Math.log(threeBVPerSecond) / Math.log(props.time)
+    const usefulClicksPerSecond = props.usefulClicks / props.time
+    const clicksPerSecond = totalClicks / props.time
+    const efficiency = props.current3BV / totalClicks
+    const throughput = props.current3BV / props.usefulClicks
+    const correctness = props.usefulClicks / totalClicks
     return (
         <div className="gameResult--container">
-            <div>
-                Time: {`${props.time}s`}
+            <div className="gameResult--stats">
+                <div>Statistics</div>
+                <div className="gameResult--time-stats">
+                    <div className="gameResult--title">Time</div>
+                    <div>
+                        Time: {`${props.time}s`}
+                    </div>
+                    <div>
+                        Estimated Time: {`${Math.round(1000 * estimatedTime) / 1000}s`}
+                    </div>
+                </div>
+                <div className="gameResult--3BV-stats">
+                    <div className="gameResult--title">Progress</div>
+                    <div>
+                        3BV: {props.threeBV}
+                    </div>
+                    <div>
+                        Current 3BV: {props.current3BV}
+                    </div>
+                    <div>Game Progress: {`${Math.round(100 * gameProgress)}%`}</div>
+                </div>
+                <div className="gameResult--click-stats">
+                    <div className="gameResult--title">Clicks</div>
+                    <div>
+                        Useful Clicks: {props.usefulClicks}
+                    </div>
+                    <div>
+                        Wasted Clicks: {props.wastedClicks}
+                    </div>
+                    <div>
+                        Total Clicks: {totalClicks}
+                    </div>
+                </div>
             </div>
-            <div>
-                3BV: {props.threeBV}
-            </div>
-            <div>
-                Current 3BV: {props.current3BV}
-            </div>
-            <div>
-                3BV/s: {Math.round(100 * threeBVPerSecond) / 100}
-            </div>
-            <div>
-                Useful Clicks: {props.usefulClicks}
-            </div>
-            <div>
-                Wasted Clicks: {props.wastedClicks}
-            </div>
-            <div>
-                Efficiency: {`${Math.round(efficiency)}%`}
+            <div className="gameResult--metrics">
+                <div>Performance</div>
+                <div className="gameResult--speed-metric">
+                    <div className="gameResult--title">Speed</div>
+                    <div>
+                        3BV/s: {Math.round(1000 * threeBVPerSecond) / 1000}
+                    </div>
+                    <div>
+                        RQP: {Math.round(1000 * RQP) / 1000}
+                    </div>
+                    <div>
+                        IOS: {Math.round(1000 * IOS) / 1000}
+                    </div>
+                </div>
+                <div className="gameResult--clicks-metric">
+                    <div className="gameResult--title">Clicking speed</div>
+                    <div>
+                        Clicks/s: {Math.round(1000 * clicksPerSecond) / 1000}
+                    </div>
+                    <div>
+                        Useful Clicks/s: {Math.round(1000 * usefulClicksPerSecond) / 1000}
+                    </div>
+                </div>
+                <div className="gameResult--efficiency-metric">
+                    <div className="gameResult--title">Efficiency</div>
+                    <div>
+                        Efficiency: {`${Math.round(100 * efficiency)}%`}
+                    </div>
+                    <div>
+                        Throughput: {`${Math.round(100 * throughput)}%`}
+                    </div>
+                    <div>
+                        Correctness: {`${Math.round(100 * correctness)}%`}
+                    </div>
+                </div>
             </div>
         </div>
-
     )
 }
