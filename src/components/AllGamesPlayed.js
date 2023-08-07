4e8f40f4c70fe69502c99061b8f5b106c0d6e112
logical-1985516 <ngyanjie0418@gmail.com>
2023-08-07 22:38:24 +0800

    Add ability to change font size in AllGamesPlayed to fit the long table
    This new feature allows all the columns to fit on the screen neatly, by
    reducing the font size. Also allows larger font size for fewer columns.
    The styles used is similar to that of the dropdowns in Settings.
    Similar to the dropdowns in Settings, a state variable, oldGamesFontSize
    is added, and the font size of the table depends on oldGamesFontSize.
    The creation of the button and dropdown is analogous to the dropdown in
    Settings, just with different names and values. In fact, it is simpler
    as the states are all managed within AllGamesPlayed: no prop drilling
    needed. (Also an update to the previous commit: similarly, this also
    saves the settings in localStorage, and retrieves the data upon reload.)

--- src/components/AllGamesPlayed.js
+++ src/components/AllGamesPlayed.js
@@ -1,11 +1,16 @@
 import React from "react"
 import OldGameResult from "./OldGameResult"
+import { nanoid } from "nanoid"
 import { resultsCollection } from "../firebase"
 import { onSnapshot } from "firebase/firestore"
 
 export default function AllGamesPlayed(props) {
     const showMetricsData = props.showMetricsData
     const [gamesResults, setGamesResults] = React.useState("")
+    const [oldGamesFontSize, setOldGamesFontSize] = React.useState(
+        JSON.parse(localStorage.getItem("oldGamesFontSize")) || 16
+    )
+    const [showFontSizeDropdown, setShowFontSizeDropdown] = React.useState(false)
 
     React.useEffect(() => {
         const unsubscribe = onSnapshot(resultsCollection, snapshot => {
@@ -18,6 +23,10 @@ export default function AllGamesPlayed(props) {
         return unsubscribe
     }, [])
 
+    function toggleFontSizeDropdown() {
+        setShowFontSizeDropdown(oldShowFontSizeDropdown => !oldShowFontSizeDropdown)
+    }
+
     function retrieveGameData(newBoard, difficulty, height, width, mines, correctFlags, time, 
         threeBV, current3BV, usefulLeftClicks, usefulRightClicks, usefulChords, wastedLeftClicks, 
         wastedRightClicks, wastedChords) {
@@ -25,6 +34,18 @@ export default function AllGamesPlayed(props) {
             threeBV, current3BV, usefulLeftClicks, usefulRightClicks, usefulChords, 
             wastedLeftClicks, wastedRightClicks, wastedChords)
     }
+
+    const fontSizes = [10, 11, 12, 13, 14, 15, 16, 18, 20, 22, 24]
+
+    React.useEffect(() => {
+        localStorage.setItem("oldGamesFontSize")
+    }, [oldGamesFontSize])
+
+    const changeFontSizeElements = fontSizes.map(fontsize =>
+        <div key={nanoid()}
+            onClick={() => setOldGamesFontSize(fontsize)}
+            style={{backgroundColor: fontsize === oldGamesFontSize ? "lightblue" : "none"}}
+            className="settings--tile-size">{fontsize}</div>)
 
     const oldGameResultElements = gamesResults && gamesResults.map(gameResult => 
         <OldGameResult 
@@ -35,27 +56,44 @@ export default function AllGamesPlayed(props) {
         />
     )
 
+    const styles = {
+        fontSize: `${oldGamesFontSize}px`
+    }
+
     return (
-        <table>
-            <th>View</th>
-            <th>Difficulty</th>
-            <th>Board</th>
-            <th>Time</th>
-            <th>Est Time</th>
-            <th>3BV</th>
-            <th>Useful Clicks</th>
-            <th>Wasted Clicks</th>
-            <th>Clicks</th>
-            <th>3BV/s</th>
-            {showMetricsData.showRQP &&<th>RQP</th>}
-            {showMetricsData.showIOS && <th>IOS</th>}
-            {showMetricsData.showClicksPerSecond && <th>CL/s</th>}
-            {showMetricsData.showUsefulClicksPerSecond && <th>UCL/s</th>}
-            <th>Eff</th>
-            {showMetricsData.showThroughput && <th>Thrp</th>}
-            {showMetricsData.showCorrectness && <th>Corr</th>}
-            <th>Date</th>
-            {oldGameResultElements}
-        </table>
+        <div>
+            <div style={{marginBottom: "10px"}}>
+                <div className="settings--tile-container">
+                    <span>Font Size: {oldGamesFontSize}</span>
+                    <div>
+                        <button onClick={toggleFontSizeDropdown}>Select</button>
+                        {showFontSizeDropdown && <div className="settings--tile-sizes-container">
+                            {changeFontSizeElements}
+                        </div>}
+                    </div>
+                </div>
+            </div>
+            <table style={styles}>
+                <th>View</th>
+                <th>Difficulty</th>
+                <th>Board</th>
+                <th>Time</th>
+                <th>Est Time</th>
+                <th>3BV</th>
+                <th>Useful Clicks</th>
+                <th>Wasted Clicks</th>
+                <th>Clicks</th>
+                <th>3BV/s</th>
+                {showMetricsData.showRQP &&<th>RQP</th>}
+                {showMetricsData.showIOS && <th>IOS</th>}
+                {showMetricsData.showClicksPerSecond && <th>CL/s</th>}
+                {showMetricsData.showUsefulClicksPerSecond && <th>UCL/s</th>}
+                <th>Eff</th>
+                {showMetricsData.showThroughput && <th>Thrp</th>}
+                {showMetricsData.showCorrectness && <th>Corr</th>}
+                <th>Date</th>
+                {oldGameResultElements}
+            </table>
+        </div>
     )
 }
