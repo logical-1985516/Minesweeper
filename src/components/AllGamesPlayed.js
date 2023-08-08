41801deecff8ecfd3ddc507c7ee6b9f90d1f7025
logical-1985516 <ngyanjie0418@gmail.com>
2023-08-08 16:59:41 +0800

    Add a way to filter results by game mode, stop text overflow in dropdown
    The filtering is done by sending a query to firebase. If the filter is
    equal to "All", remain as it is, otherwise filter based on the selected
    filter, the state gameModeFilter. Updated the onSnapShot useEffect to
    run whenever gameModeFilter is updated. Edited CSS as the text in the
    dropdown for filtering by game mode is overflowing.

--- src/components/AllGamesPlayed.js
+++ src/components/AllGamesPlayed.js
@@ -2,7 +2,7 @@ import React from "react"
 import OldGameResult from "./OldGameResult"
 import { nanoid } from "nanoid"
 import { resultsCollection } from "../firebase"
-import { onSnapshot, getDocs, query, where, orderBy } from "firebase/firestore"
+import { onSnapshot, getDocs, query, where, orderBy, and } from "firebase/firestore"
 
 export default function AllGamesPlayed(props) {
     const showMetricsData = props.showMetricsData
@@ -12,15 +12,19 @@ export default function AllGamesPlayed(props) {
     )
     const [showDropdown, setShowDropdown] = React.useState("")
     const [outcomeFilter, setOutcomeFilter] = React.useState("All")
+    const [gameModeFilter, setGameModeFilter] = React.useState("All")
     //const [gameModes, setGameModes] = React.useState("")
     // const q = outcomeFilter === "All" 
     //     ? resultsCollection
     //     : query(resultsCollection, where("outcome", "==", outcomeFilter))
 
     const sortedResults = query(resultsCollection, orderBy("date", "desc"))
+    const q = gameModeFilter === "All"
+        ? sortedResults
+        : query(sortedResults, where("gameMode", "==", gameModeFilter))
 
     React.useEffect(() => {
-        const unsubscribe = onSnapshot(sortedResults, snapshot => {
+        const unsubscribe = onSnapshot(q, snapshot => {
             const resultsArr = snapshot.docs.map(doc => ({
                 ...doc.data(),
                 id: doc.id
@@ -28,7 +32,7 @@ export default function AllGamesPlayed(props) {
             setGamesResults(resultsArr)
         })
         return unsubscribe
-    }, [])
+    }, [gameModeFilter])
 
     function toggleDropdown(name) {
         showDropdown === name
@@ -39,6 +43,11 @@ export default function AllGamesPlayed(props) {
     function changeOldGamesFontSize(newFontSize) {
         setOldGamesFontSize(newFontSize)
         toggleDropdown("fontSize")
+    }
+
+    function changeGameModeFilter(newGameMode) {
+        setGameModeFilter(newGameMode)
+        toggleDropdown("gameModeFilter")
     }
 
     function changeOutcomeFilter(newOutcome) {
@@ -66,11 +75,202 @@ export default function AllGamesPlayed(props) {
             style={{backgroundColor: fontsize === oldGamesFontSize ? "lightblue" : "none"}}
             className="dropdown-item">{fontsize}</div>)
 
+    const gameModeFilterElements = ["All", "Classic", "newGameMode"].map(gameMode =>
+        <div key={nanoid()}
+        onClick={() => changeGameModeFilter(gameMode)}
+        style={{backgroundColor: gameMode === gameModeFilter ? "lightblue" : "none"}}
+        className="dropdown-item">{gameMode}</div>)
+
     // const outcomeFilterElements = ["All", "Win", "Loss"].map(outcome => 
     //     <div key={nanoid()}
     //         onClick={() => changeOutcomeFilter(outcome)}
     //         style={{backgroundColor: outcome === outcomeFilter ? "lightblue" : "none"}}
     //         className="dropdown-item">{outcome}</div>)
+
+    // function createBoard(boardData, height, width) {
+    //     const newBoard = []
+    //     let curr = 0
+    //     for (let i = 0; i < height; i++) {
+    //         newBoard.push([])
+    //         for (let j = 0; j < width; j++) {
+    //             newBoard[i].push(boardData[curr])
+    //             curr++
+    //         }
+    //     }
+    //     return newBoard
+    // }
+
+    // function findMines(newBoard) {
+    //     let mines = 0
+    //     for (let i = 0; i < newBoard.length; i++) {
+    //         for (let j = 0; j < newBoard[0].length; j++) {
+    //             if (newBoard[i][j].value === "*") {
+    //                 mines++
+    //             }
+    //         }
+    //     }
+    //     return mines
+    // }
+
+    // function findCorrectFlags(newBoard) {
+    //     if (threeBV === current3BV) {
+    //         return mines
+    //     }
+    //     let correctFlags = 0
+    //     for (let i = 0; i < newBoard.length; i++) {
+    //         for (let j = 0; j < newBoard[0].length; j++) {
+    //             if (newBoard[i][j].isFlagged && newBoard[i][j].value === "*") {
+    //                 correctFlags++
+    //             }
+    //         }
+    //     }
+    //     return correctFlags
+    // }
+
+    // const oldGameResults = gamesResults.map(gameResult => {
+    //     const width = gameResult.width
+    //     const height = gameResult.board.length / gameResult.width
+    //     const newBoard = createBoard(gameResult.board, height, width)
+    //     const mines = findMines(newBoard)
+    //     const threeBV = find3BV(newBoard)
+    //     const current3BV = findCurrent3BV(newBoard)
+    //     const correctFlags = findCorrectFlags(newBoard)
+    //     function createBoard() {
+    //         const newBoard = []
+    //         let curr = 0
+    //         for (let i = 0; i < height; i++) {
+    //             newBoard.push([])
+    //             for (let j = 0; j < width; j++) {
+    //                 newBoard[i].push(gameResult.board[curr])
+    //                 curr++
+    //             }
+    //         }
+    //         return newBoard
+    //     }
+    
+    //     function findMines() {
+    //         let mines = 0
+    //         for (let i = 0; i < height; i++) {
+    //             for (let j = 0; j < width; j++) {
+    //                 if (newBoard[i][j].value === "*") {
+    //                     mines++
+    //                 }
+    //             }
+    //         }
+    //         return mines
+    //     }
+    
+    //     function findCorrectFlags() {
+    //         if (threeBV === current3BV) {
+    //             return mines
+    //         }
+    //         let correctFlags = 0
+    //         for (let i = 0; i < height; i++) {
+    //             for (let j = 0; j < width; j++) {
+    //                 if (newBoard[i][j].isFlagged && newBoard[i][j].value === "*") {
+    //                     correctFlags++
+    //                 }
+    //             }
+    //         }
+    //         return correctFlags
+    //     }
+    
+    //     function find3BV() {
+    //         const visited = []
+    //         let threeBV = 0
+    //         for (let i = 0; i < height; i++) {
+    //             visited.push([])
+    //             for (let j = 0; j < width; j++) {
+    //                 visited[i].push(false)
+    //             }
+    //         }
+    //         function DFS(row, col) {
+    //             if (row < 0 || row === height || col < 0 || col === width ||
+    //                 visited[row][col]) {
+    //                 return
+    //             }
+    //             visited[row][col] = true
+    //             if (newBoard[row][col].value === 0) {
+    //                 DFS(row - 1, col - 1)
+    //                 DFS(row, col - 1)
+    //                 DFS(row + 1, col - 1)
+    //                 DFS(row - 1, col)
+    //                 DFS(row + 1, col)
+    //                 DFS(row - 1, col + 1)
+    //                 DFS(row, col + 1)
+    //                 DFS(row + 1, col + 1)
+    //             }
+    //         }
+    //         for (let i = 0; i < height; i++) {
+    //             for (let j = 0; j < width; j++) {
+    //                 if (newBoard[i][j].value === 0 && !visited[i][j]) {
+    //                     DFS(i, j)
+    //                     threeBV++
+    //                 }
+    //             }
+    //         }
+    //         for (let i = 0; i < height; i++) {
+    //             for (let j = 0; j < width; j++) {
+    //                 if (!visited[i][j] && newBoard[i][j].value !== "*") {
+    //                     threeBV++
+    //                 }
+    //             }
+    //         }
+    //         return threeBV
+    //     }
+    
+    //     function findCurrent3BV() {
+    //         const visited = []
+    //         let threeBV = 0
+    //         let visitedRevealed = false
+    //         for (let i = 0; i < height; i++) {
+    //             visited.push([])
+    //             for (let j = 0; j < width; j++) {
+    //                 visited[i].push(false)
+    //             }
+    //         }
+    //         function DFS(row, col) {
+    //             if (row < 0 || row === height || col < 0 || col === width ||
+    //                 visited[row][col]) {
+    //                 return
+    //             }
+    //             visited[row][col] = true
+    //             if (newBoard[row][col].isRevealed) {
+    //                 visitedRevealed = true
+    //             }
+    //             if (newBoard[row][col].value === 0) {
+    //                 DFS(row - 1, col - 1)
+    //                 DFS(row, col - 1)
+    //                 DFS(row + 1, col - 1)
+    //                 DFS(row - 1, col)
+    //                 DFS(row + 1, col)
+    //                 DFS(row - 1, col + 1)
+    //                 DFS(row, col + 1)
+    //                 DFS(row + 1, col + 1)
+    //             }
+    //         }
+    //         for (let i = 0; i < height; i++) {
+    //             for (let j = 0; j < width; j++) {
+    //                 if (newBoard[i][j].value === 0 && !visited[i][j]) {
+    //                     visitedRevealed = false
+    //                     DFS(i, j)
+    //                     visitedRevealed && threeBV++
+    //                 }
+    //             }
+    //         }
+    //         for (let i = 0; i < height; i++) {
+    //             for (let j = 0; j < width; j++) {
+    //                 if (!visited[i][j] && newBoard[i][j].value !== "*" && newBoard[i][j].isRevealed) {
+    //                     threeBV++
+    //                 }
+    //             }
+    //         }
+    //         return threeBV
+    //     }
+    //     return {
+    //         outcome: current3BV === threeBV ? "Win" : "Loss",
+
+    // }})
 
     const oldGameResultElements = gamesResults && gamesResults.map(gameResult => 
         <OldGameResult 
@@ -98,9 +298,9 @@ export default function AllGamesPlayed(props) {
                     </div>
                 </div>
             </div>
-            {/* <div>
+            <div>
                 <div style={{marginBottom: "10px", textDecoration: "underline"}}>Filters</div>
-                <div style={{marginBottom: "5px"}}>
+                {/* <div style={{marginBottom: "5px"}}>
                     <div className="label-and-dropdown">
                         <span>Outcome: {outcomeFilter}</span>
                         <div>
@@ -110,8 +310,19 @@ export default function AllGamesPlayed(props) {
                             </div>}
                         </div>
                     </div>
+                </div> */}
+                <div style={{marginBottom: "5px"}}>
+                    <div className="label-and-dropdown">
+                        <span>Game Mode: {gameModeFilter}</span>
+                        <div>
+                            <button onClick={() => toggleDropdown("gameModeFilter")}>Select</button>
+                            {showDropdown === "gameModeFilter" && <div className="dropdown-container">
+                                {gameModeFilterElements}
+                            </div>}
+                        </div>
+                    </div>
                 </div>
-            </div> */}
+            </div>
             <table style={styles}>
                 <th>View</th>
                 <th>Outcome</th>
